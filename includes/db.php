1e6a4c94e02bd64f3ac0ea3f104e9a9402a556ec
David Watson <dave.watson8001@gmail.com>
2018-02-21 18:57:35 +0000

    database update

--- includes/db.php
+++ includes/db.php
@@ -7,23 +7,30 @@ $db['db_name'] = "cms";
 $db['db_port'] = "8889";*/
 
 
+$db['db_host'] = "us-cdbr-iron-east-05.cleardb.net";
+$db['db_user'] = "b2e6fde5c104d3";
+$db['db_pass'] = "2ccf37cb";
+$db['db_name'] = "heroku_881f91971a99a01";
+
+$connection = new mysqli($server, $username, $password, $db);
+
 //online settings
-$url = parse_url(getenv("mysql://b2e6fde5c104d3:2ccf37cb@us-cdbr-iron-east-05.cleardb.net/heroku_881f91971a99a01?reconnect=true"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-
+//$url = parse_url(getenv("mysql://b2e6fde5c104d3:2ccf37cb@us-cdbr-iron-east-05.cleardb.net/heroku_881f91971a99a01?reconnect=true"));
+//
+//$server = $url["host"];
+//$username = $url["user"];
+//$password = $url["pass"];
+//$db = substr($url["path"], 1);
+//
+//$conn = new mysqli($server, $username, $password, $db);
+//
 //foreach($db as $key => $value){
-    define(strtoupper($key), $value);
-}
+//    define(strtoupper($key), $value);
+//}
+//
+//    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
-    //$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
-
-    /* if($connection){
-        echo "We are connected";
-    }*/
+     //if($connection){
+      //  echo "We are connected";
+    //}
 ?>
