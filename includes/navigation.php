ce1510519567262431585c732650322207e2cf5d
David Watson <dave.watson8001@gmail.com>
2018-02-21 21:02:51 +0000

    online

--- includes/navigation.php
+++ includes/navigation.php
@@ -9,21 +9,21 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="../index">Homepage</a>
+            <a class="navbar-brand" href="../">Homepage</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
 
-               <li><a href="contacts.php">Contact Us</a></li>
+                <li><a href="contacts.php">Contact Us</a></li>
                 <?php
-/*                $query = "SELECT * FROM categories";
+                $query = "SELECT * FROM categories";
                 $select_all_categories_query = mysqli_query($connection, $query); 
                 while($row = mysqli_fetch_assoc($select_all_categories_query)){
                     $cat_title = $row['cat_title'];
                     echo "<li><a href = '#'>{$cat_title}</a></li>";
-                }*/
-/*                if(isset($_SESSION['user_role'])){
+                }
+                if(isset($_SESSION['user_role'])){
                     if($_SESSION['user_role'] == 'admin'){
                         echo "<li><a href='admin'>CMS Admin</a></li>";
                     }
@@ -36,7 +36,7 @@
                         $the_post_id = $_GET['p_id'];
                         echo "<li><a href='admin/posts.php?source=edit_post&p_id={$the_post_id}'>Edit Post</a></li>";
                     }
-                }*/
+                }
                 ?> 
             </ul>
         </div>
