fff40fa4fb540eb40bf6b15efc1068c91f354aa5
David Watson <dave.watson8001@gmail.com>
2018-02-22 09:36:04 +0000

    update

--- index.php
+++ index.php
@@ -22,14 +22,15 @@
                     } else{
                         $page_1 = ($page *5) - $per_page;
                     }
-                    if($_SESSION['username'])&& ($_SESSION['user_role'] == 'admin'){
-                        //if($_SESSION['user_role'] == 'admin'){
-                        $post_query_count = "SELECT * FROM posts";
+                    if($_SESSION['username']){
+                        if($_SESSION['user_role'] == 'admin'){
+                            $post_query_count = "SELECT * FROM posts";
                         }
                         else {
                             $post_query_count = "SELECT * FROM posts WHERE post_status = 'published'";
+                        }} else {
+                        $post_query_count = "SELECT * FROM posts WHERE post_status = 'published'";
                     }
-
                     $find_count = mysqli_query($connection, $post_query_count);
                     $count = mysqli_num_rows($find_count);
 
