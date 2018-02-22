c44356239a3960ec8bb02923bfaa79b78f05102c
David Watson <dave.watson8001@gmail.com>
2018-02-22 11:23:53 +0000

    update

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -1,7 +1,6 @@
 <!-- jQuery -->
     
     <script src="../admin/js/scripts.js"></script>
-    <?php echo "YOYO";?>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="../admin/js/bootstrap.min.js"></script>
