eae5ae7046cd62c90d8b40ba34d2b3bbdccdfc15
David Watson <dave.watson8001@gmail.com>
2018-02-22 15:50:02 +0000

    updates

--- includes/login.php
+++ includes/login.php
@@ -1,3 +1,6 @@
+<?php  include "includes/db.php"; ?>
+<?php  include "includes/header.php"; ?>
+<?php  include "includes/navigation.php"; ?>
 
 <!-- Page Content -->
 <div class="container">
@@ -36,4 +39,5 @@
         </div>
     </div>
     <hr>
+    <?php include "includes/footer.php";?>
 </div> <!-- /.container -->
