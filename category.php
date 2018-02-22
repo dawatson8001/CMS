a9bcbe0e730f6e6e5ae28ca3d7669f841f7ec599
David Watson <dave.watson8001@gmail.com>
2018-02-22 16:57:18 +0000

    updates

--- category.php
+++ category.php
@@ -52,7 +52,8 @@
                     <p><?php echo $post_content; ?></p>
                     <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
                     <hr>
-                    
+                    <?php
+                        } ?>
                                         <h1 class="page-header">
                         <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                     </h1>
