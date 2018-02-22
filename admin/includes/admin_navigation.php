e127c297a657422818cf49993977f6ac123d5f91
David Watson <dave.watson8001@gmail.com>
2018-02-22 13:17:26 +0000

    updates

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -11,7 +11,7 @@
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-left top-nav">
-                <li><a href="">Users Online: <span class="usersonline"></span></a></li>
+                <!--<li><a href="">Users Online: <span class="usersonline"></span></a></li>-->
             </ul>
             <ul class="nav navbar-right top-nav">
                <li><a href="../index">HOME SITE</a></li>  
