fbd065d690338afa8f71aa10f02914a8b4b2d8fa
David Watson <dave.watson8001@gmail.com>
2018-02-23 10:12:31 +0000

    update

--- category.php
+++ category.php
@@ -35,7 +35,7 @@
                         //if(!mysqli_stmt_fetch($stmt)){
                           //  echo "<h1 class='text-center'>No Post available for this category</h1>";
                         //}
-                    <?php
+                    
                             while(mysqli_stmt_fetch($stmt)){
 
                     ?>
