c68d08c1ff7e0b56b816e043fec1199d6a78c594
David Watson <dave.watson8001@gmail.com>
2018-02-23 13:53:41 +0000

    update

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -83,7 +83,7 @@ if(isset($_POST['checkBoxArray'])){
                 <th>Views</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody id="mainbody">
             <?php
 
             $query = "SELECT posts.post_id, posts.post_author, posts.post_user, posts.post_title, posts.post_category_id, posts.post_status, posts.post_image, posts.post_tags, posts.post_comment_count, posts.post_date, posts.post_views_count,  categories.cat_id, categories.cat_title";
