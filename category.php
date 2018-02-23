28ef6fc0757195c8852a938954e015ad7570ccf8
David Watson <dave.watson8001@gmail.com>
2018-02-23 09:46:37 +0000

    update

--- category.php
+++ category.php
@@ -52,21 +52,7 @@
                     <p><?php echo $post_content; ?></p>
                     <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
                     <hr>
-                    <?php
-                        } ?>
-                                        <h1 class="page-header">
-                        <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
-                    </h1>
-                    <p class="lead">
-                        by <a href="#"><?php echo $post_author; ?></a>
-                    </p>
-                    <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
-                    <hr>
-                    <img width="300" class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                    <hr>
-                    <p><?php echo $post_content; ?></p>
-                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                    <hr>
+                    
                     <?php
                         }
                         
