0efd7090d4d9f4e2fb26a4318af417efa036893a
david watson <dave.watson8001@gmail.com>
2018-09-21 14:38:50 +0100

    db settings

--- includes/db.php
+++ includes/db.php
@@ -1,16 +1,16 @@
 <?php ob_start();
 
-/*$db['db_host'] = "localhost";
-$db['db_user'] = "root";
-$db['db_pass'] = "root";
-$db['db_name'] = "cms";
-$db['db_port'] = "8889";*/
+// $db['db_host'] = "localhost";
+// $db['db_user'] = "cmsadmin";
+// $db['db_pass'] = "73svco25FlzENyJ5";
+// $db['db_name'] = "cms";
+// $db['db_port'] = "8889";
 
 
-$db['db_host'] = "us-cdbr-iron-east-05.cleardb.net";
-$db['db_user'] = "b2e6fde5c104d3";
-$db['db_pass'] = "2ccf37cb";
-$db['db_name'] = "heroku_881f91971a99a01";
+$db['db_host'] = "us-cdbr-iron-east-01.cleardb.net";
+$db['db_user'] = "b1bb19c33d3b9f";
+$db['db_pass'] = "e3290853";
+$db['db_name'] = "heroku_db9168b90eb2533";
 
 //online settings
 //$url = parse_url(getenv("mysql://b2e6fde5c104d3:2ccf37cb@us-cdbr-iron-east-05.cleardb.net/heroku_881f91971a99a01?reconnect=true"));
